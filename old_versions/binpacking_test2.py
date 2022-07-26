6cab297465e03e6d7de075ed4f5d062d0d85285c
livvyplaterink <livvyplaterink@berkeley.edu>
2022-07-26 15:50:42 -0700

    moving binpacking_test2 to old versions

--- old_versions/binpacking_test2.py
+++ old_versions/binpacking_test2.py
@@ -1,47 +1,56 @@
 from sorting_objects import *
 from item_bin import *
-from binpacking_heuristic import *
+from binpacking_heuristic7 import *
 import sys
 import random
 import timeit
+import cProfile
+import pstats
+import io
 
-start = timeit.default_timer()
+#start = timeit.default_timer()
+
+pr = cProfile.Profile()
+pr.enable()
 
 #defining cost/volume ratio for bins
 for b in bin_objects:
     b.cv_ratio = b.cost / b.volume
 
 #ordering bins in K according to non-decreasing order of the ratio c_j/V_j and non-decreasing order of V_j when the cost ratios c_j/V_j are equal
-ordered_bins = multisort(bin_objects, (('cv_ratio', False), ('volume', False)))
+original_bin_list = bin_objects.copy()
+#original_bin_list.sort(key = attrgetter('cv_ratio', 'volume'))
+original_bin_list.sort(key = lambda x: (x.cv_ratio, x.volume))
+ordered_bins = original_bin_list.copy()
 
 #ordering items in I according to non-increasing order of volume
 ordered_items = sorted(item_objects, key = attrgetter('volume'), reverse = True)
 
-print(ordered_bins)
 print(ordered_items)
+print(ordered_bins)
+
+start1 = timeit.default_timer()
 
 #running heuristic on ordered items 
 base_result = packing_bins(ordered_items, ordered_bins)
 base_cost = base_result.cost
 base_num = len(base_result.filled_bins) 
 
-print("base result: " + str(base_result)) 
+end1 = timeit.default_timer()
+
+print("base result: " + str(base_result.filled_bins)) 
 print("base cost: " + str(base_cost))
+print(len(base_result.filled_bins))
+print(end1-start1)
 
-#checking that all of the items are packed into the bins 
-bin_items = []
-for b in base_result.filled_bins:
-    for i in b.items_contained:
-        bin_items.append(i)
+#sys.exit()
 
-for i in item_objects:
-    if i not in bin_items:
-        print("ERROR!!!! ISSUE WITH ITEMS IN BINS")
-        print(i)
+if base_cost == -1: 
+    sys.exit() 
 
 #defining p, number of permutations
 p = 0.05
-num_permutations = 2000
+num_permutations = 600
 base_ordering = ordered_items.copy() 
 num_improved = 0
 replacements = []
@@ -53,12 +62,17 @@ for j in range(num_permutations):
     n = len(item_objects)
     new_item_order = []
 
+    ordered_bins = original_bin_list.copy()
+
     #resetting the items contained and remaining volume in the bins 
     for b in ordered_bins:
         b.items_contained = []
         b.remaining_volume = b.volume
+    #print(ordered_bins)
 
     #print(base_ordering) 
+    
+    start4 = timeit.default_timer()
 
     #creating a new ordering of items 
     while n > 0:
@@ -69,39 +83,49 @@ for j in range(num_permutations):
                 #print("yes") 
                 #print(base_ordering[i])
                 new_item_order.append(base_ordering[i])
+                #base_ordering[:] = [x for x in base_ordering if not base_ordering[i]]
+                #base_ordering = base_ordering[0:i] + base_ordering[i+1:]
                 base_ordering.remove(base_ordering[i])
+                #del base_ordering[i]
+                #base_ordering2 = base_ordering[:i] + base_ordering[i+1:]
+                #base_ordering = base_ordering2
+                #print(len(base_ordering))
                 n-=1
+                #print(n)
                 break
             else: 
                 #print("no")
                 continue 
     
+    end4 = timeit.default_timer()
+
+    print(end4-start4)
+
     #print(new_item_order)
+
+    start2 = timeit.default_timer()
 
     #running heuristic on new ordering of items 
     new_result = packing_bins(new_item_order, ordered_bins)
     new_cost = new_result.cost
     new_filled_bins = new_result.filled_bins
 
-    #print("new result " + str(new_result)) 
-    #print("new cost " + str(new_cost)) 
-    #print("new_filled_bins " + str(new_filled_bins)) 
-    #print("base result " + str(base_result)) 
-    #print("base cost: " + str(base_cost))
-    #print("new item order " + str(new_item_order)) 
+    end2 = timeit.default_timer()
+
 
     #checking that all items are packed 
     bin_items = []
     bin_error = False
-    for b in new_filled_bins: 
-        for i in b.items_contained:
-            bin_items.append(i)
-    for i in item_objects: 
-        if i not in bin_items: 
-            bin_error = True  
-            print("ERROR!!!! ISSUE WITH ITEMS IN BINS")
-            break
-    print(bin_error)
+    if new_cost == -1: 
+        bin_error = True 
+    #for b in new_filled_bins: 
+    #    bin_items += b.items_contained
+    #for i in item_objects: 
+    #    if i not in bin_items: 
+    #        bin_error = True  
+    #        print("ERROR!!!! ISSUE WITH ITEMS IN BINS")
+    #        break
+    #print(bin_error)
 
     #redefining base result if the new cost < base cost 
     if new_cost < base_cost: 
@@ -129,6 +153,7 @@ for j in range(num_permutations):
         print(new_cost) 
         #print("base ordering end " + str(base_ordering))
         #print("current base ordering end " + str(current_base_ordering)) 
+    print(end2-start2)
     print("---------------------------------")
 
 final_num = len(base_result.filled_bins) 
@@ -138,28 +163,36 @@ print(final_num)
 print('number of replacements ', num_improved) 
 print(replacements)
 
+pr.disable()
+s = io.StringIO()
+ps = pstats.Stats(pr, stream=s).sort_stats('tottime')
+ps.print_stats()
+
+with open('testb_20000_40000.txt', 'w+') as f:
+        f.write(s.getvalue())
+
 #printing off the results and storing them in binpacking_result.py to be used by visualization.py 
-for b in ordered_bins:
-    b.items_contained = []
-    b.remaining_volume = b.volume
-base_result = packing_bins(base_ordering, ordered_bins)
-for b in ordered_bins: 
-    if b not in base_result.filled_bins:
-        base_result.filled_bins.append(b)
-result = "filled_bins = ["
-for b in base_result.filled_bins: 
-    result += "bins('" + str(b.name) + "', " + str(b.volume) + ", " + str(b.remaining_volume) + ", " + str(b.cost) + ", " + str(b.cv_ratio) + ", ["
-    for i in b.items_contained: 
-        result += "items" + str(i) + ", " 
-    result += "]), "
-result += "]" 
+#for b in ordered_bins:
+#    b.items_contained = []
+#    b.remaining_volume = b.volume
+#base_result = packing_bins(base_ordering, ordered_bins)
+#for b in ordered_bins: 
+#    if b not in base_result.filled_bins:
+#        base_result.filled_bins.append(b)
+#result = "filled_bins = ["
+#for b in base_result.filled_bins: 
+#    result += "bins('" + str(b.name) + "', " + str(b.volume) + ", " + str(b.remaining_volume) + ", " + str(b.cost) + ", " + str(b.cv_ratio) + ", ["
+#    for i in b.items_contained: 
+#        result += "items" + str(i) + ", " 
+#    result += "]), "
+#result += "]" 
 
-stop = timeit.default_timer()
+#stop = timeit.default_timer()
 
-print("time: ", stop - start)
+#print("time: ", stop - start)
 
-with open('binpacking_result.py', 'w') as f:
-    sys.stdout = f
-    print("from sorting_objects import *")
-    print(result)
+#with open('binpacking_result.py', 'w') as f:
+#    sys.stdout = f
+#    print("from sorting_objects import *")
+#    print(result)
 
